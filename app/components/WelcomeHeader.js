9dc138db2db4f207a9ca99d943d8d42e3e90a202
Zarmeen Rauf <148314324+Zarmeen1978@users.noreply.github.com>
2024-08-22 10:50:46 +0500

    change in the profile linking button

--- app/components/WelcomeHeader.js
+++ app/components/WelcomeHeader.js
@@ -1,6 +1,6 @@
 import { Image, StyleSheet, Text, View,TouchableOpacity } from 'react-native'
 import React, { useContext } from 'react'
-
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { useNavigation } from '@react-navigation/native';
 const WelcomeHeader = () => {
   const navigation=useNavigation();
@@ -9,18 +9,26 @@ const WelcomeHeader = () => {
   }
     //const {userData,setUserData} = useContext(AuthenticatorResponse)
   return (
-    <View style={styles.container}>
+    <View style
+    ={styles.container}>
       <View>
         <Text>Hello</Text>
         <Text style={{fontSize:20,fontWeight:'bold',color:'#C36FDE'}}>Ahmad</Text>
         {/* <Text>{userData?.name}</Text> */}
       </View>
       <View>
-      <Image source={require('../assets/profile.png')}
-      style={{width:40,height:40,borderRadius:100, backgroundColor:'#C36FDE',
+    
+      <FontAwesome6 name="user" size={24} color="white" style={
+        {width:'fit-to-content',borderRadius:100, 
+        backgroundColor:'#C36FDE',
+        marginLeft:'auto',padding:9
+      }}/>
+      {/* <Image source={require('../assets/profile.png')}
+      style={{width:40,height:40,borderRadius:100, 
+        backgroundColor:'#C36FDE',
         marginLeft:20,paddingBottom:12
       }}
-      />
+      /> */}
       {/* <TouchableOpacity>
         <Link href='/ProfilePgae'>
         <Text style={{color:'#C36FDE'}}>Create Profile!</Text>
